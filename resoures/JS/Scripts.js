61a632316a2951083d77ec08179176a58d910e14
Emskelton <72915017+Emskelton@users.noreply.github.com>
2020-11-22 21:03:00 +1100

    changes to css and html for my content pages

--- resoures/JS/Scripts.js
+++ resoures/JS/Scripts.js
@@ -6,6 +6,18 @@ function openNav() {
     toggler.onclick = function () {
         navigation.classList.toggle('open');
     };
-
 }
-openNav();
+openNav(); 
+
+function myFunction(imgs) {
+    // Get the expanded image
+    var expandImg = document.getElementById("expandedImg");
+    // Get the image text
+    var imgText = document.getElementById("imgtext");
+    // Use the same src in the expanded image as the image being clicked on from the grid
+    expandImg.src = imgs.src;
+    // Use the value of the alt attribute of the clickable image as text inside the expanded image
+    imgText.innerHTML = imgs.alt;
+    // Show the container element (hidden with CSS)
+    expandImg.parentElement.style.display = "block";
+  }
